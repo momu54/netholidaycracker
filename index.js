51bcbeeec5096752d5b1f96a06d9a1a178e0e27c
momu54 <96234201+momu54@users.noreply.github.com>
2023-07-04 20:16:16 +0800

    feat: Add topic into answers result

--- index.js
+++ index.js
@@ -24,145 +24,155 @@
                佛主保佑         永無BUG
 */
 
-const { app, BrowserWindow, ipcMain, dialog } = require("electron");
-const { existsSync } = require("fs");
-const { mkdir, writeFile, readFile } = require("fs/promises");
-const { join } = require("path");
-const { setTimeout } = require("timers/promises");
-let answerspath = "";
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const { existsSync } = require('fs');
+const { mkdir, writeFile, readFile } = require('fs/promises');
+const { join } = require('path');
+const { setTimeout } = require('timers/promises');
+let answerspath = '';
 switch (process.platform) {
-    case "win32":
-        answerspath = `${process.env.APPDATA}/NetHolidayCracker/`;
-        break;
-    case "darwin":
-        answerspath = `${process.env.HOME}/Library/Application Support/NetHolidayCracker/`;
-        break;
-    case "linux":
-        answerspath = `${process.env.HOME}/.config/NetHolidayCracker/`;
-        break;
-    default:
-        break;
+	case 'win32':
+		answerspath = `${process.env.APPDATA}/NetHolidayCracker/`;
+		break;
+	case 'darwin':
+		answerspath = `${process.env.HOME}/Library/Application Support/NetHolidayCracker/`;
+		break;
+	case 'linux':
+		answerspath = `${process.env.HOME}/.config/NetHolidayCracker/`;
+		break;
+	default:
+		break;
 }
 /** @type {any[]} */
 let answers = [];
 CheckFileExists();
 GetAnswerFromFile().then((answersfromfile) => {
-    answers = JSON.parse(answersfromfile);
+	answers = JSON.parse(answersfromfile);
 });
 let cracktimes = -1;
+/** @type {Map<number, string>} */
+const topicmap = new Map();
 
-app.on("ready", () => {
-    const window = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        webPreferences: {
-            preload: join(__dirname, "preload.js"),
-            nodeIntegration: true,
-        },
-    });
+app.on('ready', () => {
+	const window = new BrowserWindow({
+		width: 1280,
+		height: 720,
+		webPreferences: {
+			preload: join(__dirname, 'preload.js'),
+			nodeIntegration: true,
+		},
+	});
 
-    window.loadURL("https://netholiday.kh.edu.tw/web_index.action");
+	window.loadURL('https://netholiday.kh.edu.tw/web_index.action');
 
-    try {
-        window.webContents.debugger.attach("1.1");
-    } catch (error) {
-        console.error(error);
-    }
+	try {
+		window.webContents.debugger.attach('1.1');
+	} catch (error) {
+		console.error(error);
+	}
 
-    window.webContents.setWindowOpenHandler((handler) => {
-        window.loadURL(handler.url);
-        return {
-            action: "deny",
-        };
-    });
+	window.webContents.setWindowOpenHandler((handler) => {
+		window.loadURL(handler.url);
+		return {
+			action: 'deny',
+		};
+	});
 
-    ipcMain.on(
-        "StartCracker",
-        /** @param {number} index */
-        async (_event, index) => {
-            await setTimeout(400);
-            window.webContents.send("SelectAnswer", index);
-        }
-    );
+	ipcMain.on(
+		'StartCracker',
+		/** @param {number} index */
+		async (_event, index) => {
+			await setTimeout(400);
+			window.webContents.send('SelectAnswer', index);
+		}
+	);
 
-    ipcMain.on("ShowPopup", async (_event) => {
-        await dialog.showMessageBox(window, {
-            type: "info",
-            title: "通知",
-            message: "開始刷題\n請勿對視窗進行任何操作",
-            buttons: ["彳亍!"],
-        });
-    });
+	ipcMain.on('ShowPopup', async (_event) => {
+		await dialog.showMessageBox(window, {
+			type: 'info',
+			title: '通知',
+			message: '開始刷題\n請勿對視窗進行任何操作',
+			buttons: ['彳亍!'],
+		});
+	});
 
-    ipcMain.on("CrackDone", async (_event, index) => {
-        cracktimes++;
-        dialog.showMessageBox(window, {
-            type: "info",
-            title: "通知",
-            message: `已完成刷題 ${cracktimes + 1}/4\n請勿繼續操作!!${
-                cracktimes < 3 ? "\n請等待下一輪刷題在一分鐘後自動開始!!" : ""
-            }`,
-            buttons: ["彳亍!"],
-        });
-        if (cracktimes < 3) {
-            await setTimeout(60000);
-            window.webContents.send("ContinueCrack", index);
-        } else {
-            cracktimes = -1;
-            window.webContents.send("StartAnswer", index);
-        }
-    });
+	ipcMain.on('CrackDone', async (_event, index) => {
+		cracktimes++;
+		dialog.showMessageBox(window, {
+			type: 'info',
+			title: '通知',
+			message: `已完成刷題 ${cracktimes + 1}/4\n請勿繼續操作!!${
+				cracktimes < 3 ? '\n請等待下一輪刷題在一分鐘後自動開始!!' : ''
+			}`,
+			buttons: ['彳亍!'],
+		});
+		if (cracktimes < 3) {
+			await setTimeout(60000);
+			window.webContents.send('ContinueCrack', index);
+		} else {
+			cracktimes = -1;
+			window.webContents.send('StartAnswer', index);
+		}
+	});
 
-    window.webContents.debugger.on(
-        "message",
-        async (_event, method, params) => {
-            if (
-                method === "Network.responseReceived" &&
-                params.response.url.includes("result_list.action")
-            ) {
-                const { body } = await window.webContents.debugger.sendCommand(
-                    "Network.getResponseBody",
-                    { requestId: params.requestId }
-                );
-                /** @type {any[]} */
-                answers = [
-                    ...new Set([
-                        ...answers,
-                        ...JSON.parse(body).data.list.map((answer) => {
-                            return {
-                                id: answer.id,
-                                answer: answer.c == "Y" ? answer.s : answer.c,
-                            };
-                        }),
-                    ]),
-                ];
-            }
-        }
-    );
+	window.webContents.debugger.on('message', async (_event, method, params) => {
+		if (method !== 'Network.responseReceived') return;
+		if (params.response.url.includes('result_list.action')) {
+			const { body } = await window.webContents.debugger.sendCommand(
+				'Network.getResponseBody',
+				{ requestId: params.requestId }
+			);
+			console.log(body);
+			/** @type {{title: string, id: number, answer: string}[]} */
+			answers = [
+				...new Set([
+					...answers,
+					.../** @type {any[]} */ (JSON.parse(body).data.list).map(
+						(answer, index) => {
+							return {
+								topic: topicmap.get(index + 1),
+								id: answer.id,
+								answer: answer.c == 'Y' ? answer.s : answer.c,
+							};
+						}
+					),
+				]),
+			];
+		} else if (params.response.url.includes('answer_topic.action')) {
+			const { data } = JSON.parse(
+				(
+					await window.webContents.debugger.sendCommand(
+						'Network.getResponseBody',
+						{
+							requestId: params.requestId,
+						}
+					)
+				).body
+			);
+			topicmap.set(data.title, data.topic);
+		}
+	});
 
-    window.webContents.debugger.sendCommand("Network.enable");
+	window.webContents.debugger.sendCommand('Network.enable');
 });
 
 async function WriteAnswer() {
-    return await writeFile(
-        `${answerspath}/answers.json`,
-        JSON.stringify(answers)
-    );
+	return await writeFile(`${answerspath}/answers.json`, JSON.stringify(answers));
 }
 
 async function GetAnswerFromFile() {
-    const jsonpath = `${answerspath}/answers.json`;
-    if (!existsSync(jsonpath)) await writeFile(jsonpath, "[]");
-    return await readFile(jsonpath, {
-        encoding: "utf8",
-    });
+	const jsonpath = `${answerspath}/answers.json`;
+	if (!existsSync(jsonpath)) await writeFile(jsonpath, '[]');
+	return await readFile(jsonpath, {
+		encoding: 'utf8',
+	});
 }
 
-app.on("window-all-closed", async () => {
-    await WriteAnswer();
-    app.exit();
+app.on('window-all-closed', async () => {
+	await WriteAnswer();
+	app.exit();
 });
 
 async function CheckFileExists() {
-    if (!existsSync(answerspath)) await mkdir(answerspath);
+	if (!existsSync(answerspath)) await mkdir(answerspath);
 }
--- preload.js
+++ preload.js
@@ -24,92 +24,92 @@
                佛主保佑         永無BUG
 */
 
-const { ipcRenderer } = require("electron");
+const { ipcRenderer } = require('electron');
 /** @type {HTMLCollectionOf<HTMLDivElement>} */
 let subjects;
 /** @type {HTMLLIElement[]} */
 let answers;
 
 function sleep(delay) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, delay);
-    });
+	return new Promise(function (resolve) {
+		setTimeout(resolve, delay);
+	});
 }
 
-console.log("Preload script injected!");
-if (window.location.href.includes("exam.action")) {
-    subjects =
-        /** @type {HTMLCollectionOf<HTMLDivElement>} */
-        (document.getElementsByClassName("icon-item"));
-    const interval = setInterval(() => {
-        if (subjects.length == 8) {
-            clearInterval(interval);
-            CreateExamEventHandler();
-        }
-    }, 100);
-} else if (window.location.href.includes("result.action")) {
-    ExitExam();
+console.log('Preload script injected!');
+if (window.location.href.includes('exam.action')) {
+	subjects =
+		/** @type {HTMLCollectionOf<HTMLDivElement>} */
+		(document.getElementsByClassName('icon-item'));
+	const interval = setInterval(() => {
+		if (subjects.length == 8) {
+			clearInterval(interval);
+			CreateExamEventHandler();
+		}
+	}, 100);
+} else if (window.location.href.includes('result.action')) {
+	ExitExam();
 }
 
 async function ExitExam() {
-    await sleep(400);
-    document.getElementById("examOver")?.click();
+	await sleep(400);
+	document.getElementById('examOver')?.click();
 }
 
 function CreateExamEventHandler() {
-    [...subjects].forEach((subject, index) =>
-        subject.addEventListener("mousedown", async (event) => {
-            if (event.button != 1) return;
-            event.preventDefault();
-            subject.click();
-            ipcRenderer.send("StartCracker", index);
-        })
-    );
+	[...subjects].forEach((subject, index) =>
+		subject.addEventListener('mousedown', async (event) => {
+			if (event.button != 1) return;
+			event.preventDefault();
+			subject.click();
+			ipcRenderer.send('StartCracker', index);
+		})
+	);
 }
 
 /** @param {number} index */
 async function ClickAnswer(index) {
-    console.log(answers.length);
-    if (!document.getElementById("answer")) return;
-    ipcRenderer.send("ShowPopup");
-    for (const _answer of answers) {
-        await sleep(400);
-        /** @type {HTMLInputElement} */
-        (
-            /** @type {HTMLDivElement} */
-            (document.getElementById("answer")).firstElementChild
-                ?.firstElementChild?.firstElementChild
-        ).setAttribute("checked", "");
-        console.log("Tryed to click answer.");
-        await sleep(400);
-        /** @type {HTMLDivElement} */
-        (document.getElementsByClassName("next")[0]).click();
-        console.log("Tryed to click next button.");
-    }
-    await sleep(400);
-    /** @type {HTMLButtonElement} */
-    (document.getElementById("examOver")).click();
-    await sleep(400);
-    /** @type {HTMLButtonElement} */
-    (document.querySelector("#exam-modal-footer .btn.btn-primary")).click();
-    ipcRenderer.send("CrackDone", index);
+	console.log(answers.length);
+	if (!document.getElementById('answer')) return;
+	ipcRenderer.send('ShowPopup');
+	for (const _answer of answers) {
+		await sleep(400);
+		/** @type {HTMLInputElement} */
+		(
+			/** @type {HTMLDivElement} */
+			(document.getElementById('answer')).firstElementChild?.firstElementChild
+				?.firstElementChild
+		).setAttribute('checked', '');
+		console.log('Tryed to click answer.');
+		await sleep(400);
+		/** @type {HTMLDivElement} */
+		(document.getElementsByClassName('next')[0]).click();
+		console.log('Tryed to click next button.');
+	}
+	await sleep(400);
+	/** @type {HTMLButtonElement} */
+	(document.getElementById('examOver')).click();
+	await sleep(400);
+	/** @type {HTMLButtonElement} */
+	(document.querySelector('#exam-modal-footer .btn.btn-primary')).click();
+	ipcRenderer.send('CrackDone', index);
 }
 
-ipcRenderer.on("SelectAnswer", async (_event, index) => {
-    answers = [
-        .../** @type {HTMLCollectionOf<HTMLLIElement>} */
-        (document.getElementsByClassName("link-list")),
-    ];
-    await ClickAnswer(index);
+ipcRenderer.on('SelectAnswer', async (_event, index) => {
+	answers = [
+		.../** @type {HTMLCollectionOf<HTMLLIElement>} */
+		(document.getElementsByClassName('link-list')),
+	];
+	await ClickAnswer(index);
 });
 
 ipcRenderer.on(
-    "ContinueCrack",
-    /** @param {number} index */
-    (_event, index) => {
-        const subject = [...subjects][index];
-        console.log(index);
-        subject.click();
-        ipcRenderer.send("StartCracker", index);
-    }
+	'ContinueCrack',
+	/** @param {number} index */
+	(_event, index) => {
+		const subject = [...subjects][index];
+		console.log(index);
+		subject.click();
+		ipcRenderer.send('StartCracker', index);
+	}
 );
